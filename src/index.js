3be72949d93751bc31324904aed805884cc56654
codestudent24 <belgorod2407@yandex.ru>
2023-01-23 12:32:07 +1000

    init: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  console.log("str = " + str);
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    console.log(bracketsConfig[i]);
+  }
+
+  let index = 0;
+  for (let i = 0; i < str.length; i++) {
+    //    console.log("checking for " + str[i]);
+    //    console.log("i = " + i);
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (
+        str[i] === bracketsConfig[j][0] &&
+        str[i + 1] === bracketsConfig[j][1]
+      ) {
+        str = str.replace(`${str[i]}${str[i + 1]}`, "");
+        i = -1;
+        //        console.log("str = " + str);
+        //        console.log("i = " + i);
+      }
+    }
+  }
+  if (str == "") return true;
+  return false;
+};
